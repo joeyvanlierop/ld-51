89c399e105aa69d49167ab47b24954ab1cfb1a45
Joey Van Lierop <joey.vanlierop@me.com>
2022-09-30 19:54:21 -0600

    testststste

--- Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
+++ Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
@@ -1,43 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterInput : MonoBehaviour
 {
 
-    public Animator animator;
-    public float velocityX;
-    public float velocityY;
-    public float moveX;
-    public float moveY;
-    public float forceX;
-    public float forceY;
+  public Animator animator;
+  public float velocityX;
+  public float velocityY;
+  public float moveX;
+  public float moveY;
+  public float forceX;
+  public float forceY;
+  public InputAction controls;
 
-    private Rigidbody2D rb;
+  private Rigidbody2D rb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        this.moveX = Input.GetAxisRaw("Horizontal");
-        this.moveY = Input.GetAxisRaw("Vertical");
+  // Update is called once per frame
+  void Update()
+  {
+    this.moveX = Input.GetAxisRaw("Horizontal");
+    this.moveY = Input.GetAxisRaw("Vertical");
 
-        animator.SetBool("movingLeft", moveX < 0);
-        animator.SetBool("movingRight", moveX > 0);
-        animator.SetBool("movingUp", moveY > 0);
-        animator.SetBool("movingDown", moveY < 0);
+    animator.SetBool("movingLeft", moveX < 0);
+    animator.SetBool("movingRight", moveX > 0);
+    animator.SetBool("movingUp", moveY > 0);
+    animator.SetBool("movingDown", moveY < 0);
 
 
-    }
+  }
 
-    void FixedUpdate() {
+  void FixedUpdate()
+  {
 
-        if(this.forceX != 0) rb.AddForce(new Vector2(this.forceX, 0) * Time.deltaTime);
-        if(this.forceY != 0) rb.AddForce(new Vector2(this.forceY, 0) * Time.deltaTime);
-    }
+    if (this.forceX != 0) rb.AddForce(new Vector2(this.forceX, 0) * Time.deltaTime);
+    if (this.forceY != 0) rb.AddForce(new Vector2(this.forceY, 0) * Time.deltaTime);
+  }
+
+  private void OnEnable()
+  {
+    controls.Enable();
+  }
+
+  private void Disable()
+  {
+    controls.Disable();
+  }
 }
