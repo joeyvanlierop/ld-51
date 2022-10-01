20779477eb120a1dfd6f81c807ec45c35459c0d0
Joey Van Lierop <joey.vanlierop@me.com>
2022-09-30 20:33:17 -0600

    dwadwa

--- Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
+++ Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
@@ -12,46 +12,51 @@ public class CharacterInput : MonoBehaviour
   public float moveSpeedY = 5f;
   public InputAction movementAction;
 
-  public Rigidbody2D rb;
+  Rigidbody2D rb;
   Vector2 moveDirection = Vector2.zero;
 
-//   void Awake() {
-//     movementAction.performed += ctx => {
-//         Move(ctx.ReadValue<Vector2>());
-//         Animate(ctx.ReadValue<Vector2>());
-//     };
-//   }
+  void Start()
+  {
+    rb = gameObject.GetComponent<Rigidbody2D>();
+  }
 
-//   void Move(Vector2 velocity) {
-//     Debug.Log(velocity);
-//     rb.velocity = new Vector2(velocity.x * moveSpeedX, velocity.y * moveSpeedY);
-//   }
+  //   void Awake() {
+  //     movementAction.performed += ctx => {
+  //         Move(ctx.ReadValue<Vector2>());
+  //         Animate(ctx.ReadValue<Vector2>());
+  //     };
+  //   }
 
-//   void Animate(Vector2 moveDirection) {
-//     animator.SetBool("movingLeft", moveDirection.x < 0);
-//     animator.SetBool("movingRight", moveDirection.x > 0);
-//     animator.SetBool("movingUp", moveDirection.y > 0);
-//     animator.SetBool("movingDown", moveDirection.y < 0);
-//   }
+  //   void Move(Vector2 velocity) {
+  //     Debug.Log(velocity);
+  //     rb.velocity = new Vector2(velocity.x * moveSpeedX, velocity.y * moveSpeedY);
+  //   }
+
+  //   void Animate(Vector2 moveDirection) {
+  //     animator.SetBool("movingLeft", moveDirection.x < 0);
+  //     animator.SetBool("movingRight", moveDirection.x > 0);
+  //     animator.SetBool("movingUp", moveDirection.y > 0);
+  //     animator.SetBool("movingDown", moveDirection.y < 0);
+  //   }
 
   // Update is called once per frame
-//   void Update()
-//   {
-//     // this.moveX = Input.GetAxisRaw("Horizontal");
-//     // this.moveY = Input.GetAxisRaw("Vertical");
+  //   void Update()
+  //   {
+  //     // this.moveX = Input.GetAxisRaw("Horizontal");
+  //     // this.moveY = Input.GetAxisRaw("Vertical");
 
-//     // moveDirection = movementAction.ReadValue<Vector2>();
+  //     // moveDirection = movementAction.ReadValue<Vector2>();
 
-//     // animator.SetBool("movingLeft", moveDirection.x < 0);
-//     // animator.SetBool("movingRight", moveDirection.x > 0);
-//     // animator.SetBool("movingUp", moveDirection.y > 0);
-//     // animator.SetBool("movingDown", moveDirection.y < 0);
-//   }
+  //     // animator.SetBool("movingLeft", moveDirection.x < 0);
+  //     // animator.SetBool("movingRight", moveDirection.x > 0);
+  //     // animator.SetBool("movingUp", moveDirection.y > 0);
+  //     // animator.SetBool("movingDown", moveDirection.y < 0);
+  //   }
 
   private void FixedUpdate()
   {
-    Debug.Log(movementAction);
     moveDirection = movementAction.ReadValue<Vector2>();
+    Debug.Log(moveDirection);
 
     rb.velocity = new Vector2(moveDirection.x * moveSpeedX, moveDirection.y * moveSpeedY);
   }
