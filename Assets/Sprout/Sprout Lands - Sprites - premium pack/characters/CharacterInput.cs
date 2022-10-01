6b5890393c338c1304d1f7b66907aef97bcd5d7d
= <coleb@outlook.com>
2022-09-30 20:30:38 -0600

    dwqf

--- Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
+++ Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
@@ -15,24 +15,24 @@ public class CharacterInput : MonoBehaviour
   public Rigidbody2D rb;
   Vector2 moveDirection = Vector2.zero;
 
-  void Awake() {
-    movementAction.performed += ctx => {
-        Move(ctx.ReadValue<Vector2>());
-        Animate(ctx.ReadValue<Vector2>());
-    };
-  }
+//   void Awake() {
+//     movementAction.performed += ctx => {
+//         Move(ctx.ReadValue<Vector2>());
+//         Animate(ctx.ReadValue<Vector2>());
+//     };
+//   }
 
-  void Move(Vector2 velocity) {
-    Debug.Log(velocity);
-    rb.velocity = new Vector2(velocity.x * moveSpeedX, velocity.y * moveSpeedY);
-  }
+//   void Move(Vector2 velocity) {
+//     Debug.Log(velocity);
+//     rb.velocity = new Vector2(velocity.x * moveSpeedX, velocity.y * moveSpeedY);
+//   }
 
-  void Animate(Vector2 moveDirection) {
-    animator.SetBool("movingLeft", moveDirection.x < 0);
-    animator.SetBool("movingRight", moveDirection.x > 0);
-    animator.SetBool("movingUp", moveDirection.y > 0);
-    animator.SetBool("movingDown", moveDirection.y < 0);
-  }
+//   void Animate(Vector2 moveDirection) {
+//     animator.SetBool("movingLeft", moveDirection.x < 0);
+//     animator.SetBool("movingRight", moveDirection.x > 0);
+//     animator.SetBool("movingUp", moveDirection.y > 0);
+//     animator.SetBool("movingDown", moveDirection.y < 0);
+//   }
 
   // Update is called once per frame
 //   void Update()
@@ -48,10 +48,13 @@ public class CharacterInput : MonoBehaviour
 //     // animator.SetBool("movingDown", moveDirection.y < 0);
 //   }
 
-//   private void FixedUpdate()
-//   {
-//     // rb.velocity = new Vector2(moveDirection.x * moveSpeedX, moveDirection.y * moveSpeedY);
-//   }
+  private void FixedUpdate()
+  {
+    Debug.Log(movementAction);
+    moveDirection = movementAction.ReadValue<Vector2>();
+
+    rb.velocity = new Vector2(moveDirection.x * moveSpeedX, moveDirection.y * moveSpeedY);
+  }
 
   private void OnEnable()
   {
