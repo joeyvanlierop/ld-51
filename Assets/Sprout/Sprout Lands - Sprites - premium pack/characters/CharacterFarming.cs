4a710244b783b6759071ead6781d899c816dcac6
Joey Van Lierop <joey.vanlierop@me.com>
2022-10-01 00:49:36 -0600

    targeting

--- Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterFarming.cs
+++ Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterFarming.cs
@@ -2,32 +2,67 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
+[RequireComponent(typeof(CharacterInput))]
 public class CharacterFarming : MonoBehaviour
 {
   public Tile tilledTile;
+  public Tile tilledPreviewTile;
   public Tilemap groundTilemap;
   public Tilemap tilledTilemap;
+  public Tilemap tilledPreviewTilemap;
+  private CharacterInput characterInput;
 
   public InputAction tillAction;
+
+  private Vector3Int previousPos;
+  private const float REACH = 0.65f;
 
   void Awake()
   {
     tillAction.performed += _ => Till();
+    characterInput = gameObject.GetComponent<CharacterInput>();
   }
 
-  void OnUpdate()
+  void Update()
   {
-    var pos = groundTilemap.WorldToCell(transform.position);
-    var groundTile = groundTilemap.GetTile(pos);
+    var pos = GetTarget();
+    if (!pos.Equals(previousPos))
+    {
+      tilledPreviewTilemap.SetTile(previousPos, null);
+      tilledPreviewTilemap.SetTile(pos, tilledPreviewTile);
+      previousPos = pos;
+    }
   }
 
   void Till()
   {
-    var pos = tilledTilemap.WorldToCell(transform.position);
+    var pos = GetTarget();
     var groundTile = groundTilemap.GetTile(pos);
     if (!groundTile)
       return;
     tilledTilemap.SetTile(pos, tilledTile);
+  }
+
+  Vector3Int GetTarget()
+  {
+    var adjustedPos = transform.position;
+    switch (characterInput.direction)
+    {
+      case Direction.UP:
+        adjustedPos.y += REACH;
+        break;
+      case Direction.DOWN:
+        adjustedPos.y -= REACH;
+        break;
+      case Direction.LEFT:
+        adjustedPos.x -= REACH;
+        break;
+      case Direction.RIGHT:
+        adjustedPos.x += REACH;
+        break;
+    }
+    var pos = tilledTilemap.WorldToCell(adjustedPos);
+    return pos;
   }
 
   private void OnEnable()
