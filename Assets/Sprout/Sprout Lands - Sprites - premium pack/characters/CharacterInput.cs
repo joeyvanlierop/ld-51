4877a42bfe916a38626c74de253138bf9ff79138
= <coleb@outlook.com>
2022-09-30 19:07:49 -0600

    Added animations for movement directions

--- Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
+++ Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterInput : MonoBehaviour
 {
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class CharacterInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        animator.SetBool("movingLeft", moveX < 0);
+        animator.SetBool("movingRight", moveX > 0);
+        animator.SetBool("movingUp", moveY > 0);
+        animator.SetBool("movingDown", moveY < 0);
     }
 }
