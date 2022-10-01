8afd81b9622624a8bf08e0f112f17edd1cf661cc
= <coleb@outlook.com>
2022-09-30 19:47:14 -0600

    feag

--- Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
+++ Assets/Sprout/Sprout Lands - Sprites - premium pack/characters/CharacterInput.cs
@@ -6,22 +6,38 @@ public class CharacterInput : MonoBehaviour
 {
 
     public Animator animator;
+    public float velocityX;
+    public float velocityY;
+    public float moveX;
+    public float moveY;
+    public float forceX;
+    public float forceY;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        this.moveX = Input.GetAxisRaw("Horizontal");
+        this.moveY = Input.GetAxisRaw("Vertical");
 
         animator.SetBool("movingLeft", moveX < 0);
         animator.SetBool("movingRight", moveX > 0);
         animator.SetBool("movingUp", moveY > 0);
         animator.SetBool("movingDown", moveY < 0);
+
+
+    }
+
+    void FixedUpdate() {
+
+        if(this.forceX != 0) rb.AddForce(new Vector2(this.forceX, 0) * Time.deltaTime);
+        if(this.forceY != 0) rb.AddForce(new Vector2(this.forceY, 0) * Time.deltaTime);
     }
 }
